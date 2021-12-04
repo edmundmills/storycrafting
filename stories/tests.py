5c1640915ee1057ad2b056b71f73aec22c5d3c8f
Edmund Mills <edmund.martin.mills@gmail.com>
2021-12-03 17:22:10 -0700

    Add tests

--- stories/tests.py
+++ stories/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Story
+from .models import Story, Step, Proposal
 
 def create_story(**kwargs):
-    return Story.objects.create(**kwargs)
+    story = Story.objects.create(**kwargs)
+    step = Step.objects.create(story_id=story.id)
+    Proposal.objects.create(step_id=step.id)
+    return story
 
 class StoryIndexViewTests(TestCase):
     def test(self):
@@ -13,10 +16,24 @@ class StoryIndexViewTests(TestCase):
         self.assertContains(response, "New Story")
 
 class StoryNewViewTests(TestCase):
-    def test(self):
+    def test_new(self):
         response = self.client.get(reverse('stories:new'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "New Story")
+    
+    def test_create_valid(self):
+        self.client.post(reverse('stories:new'), {'title': 'Test Story'})
+        stories = Story.objects.all()
+        self.assertEqual(len(stories), 1)
+        steps = stories[0].steps.all()
+        self.assertEqual(len(steps), 1)
+        proposals = steps[0].proposals.all()
+        self.assertEqual(len(proposals), 1)
+
+    def test_create_no_title(self):
+        self.client.post(reverse('stories:new'), {'title': ''})
+        stories = Story.objects.all()
+        self.assertEqual(len(stories), 0)
 
 class StoryWriteViewTests(TestCase):
     def test(self):
@@ -25,3 +42,17 @@ class StoryWriteViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, story.title)
 
+    def test_no_story(self):
+        response = self.client.get(reverse('stories:write', args=(1,)))
+        self.assertEqual(response.status_code, 404)
+
+class StoryReadViewTests(TestCase):
+    def test(self):
+        story = create_story(title='Test Story')
+        response = self.client.get(reverse('stories:read', args=(story.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, story.title)
+
+    def test_no_story(self):
+        response = self.client.get(reverse('stories:read', args=(1,)))
+        self.assertEqual(response.status_code, 404)
